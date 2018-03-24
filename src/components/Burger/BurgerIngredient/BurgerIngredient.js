00117a902abff4eb3401de8ebf2dc1ebf4a33502
Gura-San <gura.san@gmail.com>
2018-03-24 19:42:36 -0400

    Burger component established, BurgerIngredient populated, burger renders ok

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
-import classes from './BurgerIngredient/BurgerIngredient.css'
+import classes from './BurgerIngredient.css'
 
 class BurgerIngredient extends Component {
   render () {
     let ingredient = null
     switch (this.props.type) {
       case ('bread-bottom'):
-        ingredient = <div>className={classes.BreadBottom}</div>
+        ingredient = <div className={classes.BreadBottom}></div>
         break
       case ('bread-top'):
         ingredient = (
