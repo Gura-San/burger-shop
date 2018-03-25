c1c30b1fd127333dd151caa225ec69456768c11c
Gura-San <gura.san@gmail.com>
2018-03-25 19:16:55 -0400

    addIngredientHandler added, adding ingredients from controls works

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -7,7 +7,8 @@ const BuildControl = props => {
     <div className={classes.BuildControl}>
       <div className={classes.Label}>{props.label}</div>
       <button className={classes.Less}>-</button>
-      <button className={classes.More}>+</button>
+      <button className={classes.More}
+        onClick={props.added}>+</button>
     </div>
   );
 };
