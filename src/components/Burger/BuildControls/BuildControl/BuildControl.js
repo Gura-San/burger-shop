cb82faf524ff47c8a410b2b46a791d656bb46af3
Gura-San <gura.san@gmail.com>
2018-03-24 22:00:40 -0400

    build controls available on screen

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -6,8 +6,8 @@ const BuildControl = props => {
   return (
     <div className={classes.BuildControl}>
       <div className={classes.Label}>{props.label}</div>
-      <button className={classes.Less}>Less</button>
-      <button className={classes.More}>More</button>
+      <button className={classes.Less}>-</button>
+      <button className={classes.More}>+</button>
     </div>
   );
 };
